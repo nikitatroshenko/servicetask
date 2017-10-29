78bf6d32340fa8d71894d3c1bd69692814ace0e4
nikitatroshenko <nikitatroshenko@gmail.com>
2017-10-29 21:53:08 +0300

    Added support for timestamps printing

--- Atroshenko/main.c
+++ Atroshenko/main.c
@@ -3,15 +3,19 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <openssl/md5.h>
+#include <time.h>
 
 #define DEFAULT_FILE_BUF_SIZE 512
 
 static int get_file_md5(FILE *in, unsigned char *digest);
+static void bytes_to_hex_str(unsigned char *bytes, size_t len, char *buf);
+static void log_record(FILE *out, const char *record);
 
 int main(int argc, char const **argv)
 {
 	size_t i;	/* loop counter */
 	unsigned char digest[MD5_DIGEST_LENGTH];
+	char digest_str[2 * MD5_DIGEST_LENGTH + 1];
 	FILE *in = fopen(argv[1], "rb");
 
 	if (in == NULL) {
@@ -23,10 +27,13 @@ int main(int argc, char const **argv)
 		return errno;
 	}
 
-	for (i = 0; i < MD5_DIGEST_LENGTH; i++) {
-		printf("%2x", digest[i]);
-	}
-	printf("\n");
+	bytes_to_hex_str(digest, MD5_DIGEST_LENGTH, digest_str);
+	log_record(stderr, digest_str);
+
+	// for (i = 0; i < MD5_DIGEST_LENGTH; i++) {
+	// 	printf("%2x", digest[i]);
+	// }
+	// printf("\n");
 
 	return EXIT_SUCCESS;
 }
@@ -57,4 +64,32 @@ int get_file_md5(FILE *in, unsigned char *digest)
 	success_call = MD5_Final(digest, &context);
 
 	return success_call;
+}
+
+void bytes_to_hex_str(unsigned char *bytes, size_t len, char *buf)
+{
+	size_t i;
+
+	for (i = 0; i < len; i++) {
+		sprintf(buf, "%2x", bytes[i]);
+		buf += 2;
+	}
+}
+
+void log_record(FILE *out, const char *record)
+{
+	time_t rawtime;
+	struct tm *tm;
+
+	time(&rawtime);
+	tm = localtime(&rawtime);
+
+	fprintf(out, "[%4d.%2d.%2d %2d:%2d:%2d] %s\n",
+			tm->tm_year + 1900,
+			tm->tm_mon + 1,
+			tm->tm_mday,
+			tm->tm_hour,
+			tm->tm_min,
+			tm->tm_sec,
+			record);
 }
